bdedc358fcc850b5ba13fecb865f009d9ffc1e6b
Mateo Druart <mateo.druart@gmail.com>
2021-07-09 15:29:16 +0200

    init comit

---
+++ zip/models.py
@@ -0,0 +1,9 @@
+from django.contrib.gis.db import models
+
+class Zipcode(models.Model):
+    code = models.CharField(max_length=5)
+    poly = models.PolygonField()
+
+class Elevation(models.Model):
+    name = models.CharField(max_length=100)
+    rast = models.RasterField()
